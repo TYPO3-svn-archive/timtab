e5f7ca981582e049a01dc766679749915c2d8190
flyguide <flyguide@735d13b6-9817-0410-8766-e36946ffe9aa>
2005-05-10 02:09:06 +0000

    finally found the typo which kept new fields from appearing in the new tab *aaargh*
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/timtab/trunk@2475 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -59,6 +59,7 @@ $tempColumns = Array (
 
 t3lib_div::loadTCA('tt_content');
 $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='layout,select_key';
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi2']='layout,select_key';
 
 t3lib_extMgm::addPlugin(Array('LLL:EXT:timtab/locallang_db.php:tt_content.list_type_pi1', $_EXTKEY.'_pi1'),'list_type');
 
@@ -68,11 +69,13 @@ $TCA['tt_news']['ctrl']['typeicons'][] = $thisExtRelPath.'icon_tx_timtab_post.gi
 $TCA['tt_news']['columns']['type']['config']['items'][] = Array('LLL:EXT:timtab/locallang_db.php:tt_news.type.I.timtab', 3);
 $TCA['tt_news']['interface']['showRecordFieldList'] .= ',tx_timtab_trackbacks,tx_timtab_tb2,tx_timtab_tb3';
 $TCA['tt_news']['types'][] = array();
-t3lib_extMgm::addToAllTCAtypes('tt_news', 'title;;1;;,datetime;;2;;1-1-1,author;;3;;,short,bodytext;;4;richtext[paste|bold|italic|underline|formatblock|class|left|center|right|orderedlist|unorderedlist|outdent|indent|link|table|image]:rte_transform[flag=rte_enabled|mode=ts];4-4-4,no_auto_pb,--div--;Relations,category,image;;;;1-1-1,imagecaption;;5;;,links;;;;2-2-2,related;;;;3-3-3,news_files;;;;4-4-4,--div--;Blog Post,timtab_trackbacks;;;;1-1-1, timtab_tb2, timtab_tb3', 3);
+
+t3lib_extMgm::addToAllTCAtypes('tt_news', 'title;;1;;,datetime;;2;;1-1-1,author;;3;;,short,bodytext;;4;richtext[paste|bold|italic|underline|formatblock|class|left|center|right|orderedlist|unorderedlist|outdent|indent|link|table|image]:rte_transform[flag=rte_enabled|mode=ts];4-4-4,no_auto_pb,--div--;Relations,category,image;;;;1-1-1,imagecaption;;5;;,links;;;;2-2-2,related;;;;3-3-3,news_files;;;;4-4-4,--div--;Blog Post,tx_timtab_trackbacks;;;;1-1-1, tx_timtab_tb2, tx_timtab_tb3', 3);
 
 t3lib_extMgm::addStaticFile($_EXTKEY,'static/kubrick/','Kubrick (default weblog template)');
 t3lib_extMgm::addStaticFile($_EXTKEY,'static/kubrick_single/','Kubrick Single View (default weblog template)');
-t3lib_extMgm::addStaticFile($_EXTKEY,'pi1/static/','blogroll');
+t3lib_extMgm::addStaticFile($_EXTKEY,'static/webservice/','Blog Webservices');
+t3lib_extMgm::addStaticFile($_EXTKEY,'pi1/static/','Blogroll');
 
 if (TYPO3_MODE=="BE")	$TBE_MODULES_EXT["xMOD_db_new_content_el"]["addElClasses"]["tx_timtab_pi1_wizicon"] = t3lib_extMgm::extPath($_EXTKEY).'pi1/class.tx_timtab_pi1_wizicon.php';
 ?>
