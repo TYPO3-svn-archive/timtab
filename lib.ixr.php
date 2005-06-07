74fc736ddfc8ec747f67cf62b32b655d621caf51
flyguide <flyguide@735d13b6-9817-0410-8766-e36946ffe9aa>
2005-06-07 22:29:17 +0000

    removed unreachable code
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/timtab/trunk@2497 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib.ixr.php
+++ lib.ixr.php
@@ -70,16 +70,12 @@ class IXR_Value {
         switch ($this->type) {
             case 'boolean':
                 return '<boolean>'.(($this->data) ? '1' : '0').'</boolean>';
-                break;
             case 'int':
                 return '<int>'.$this->data.'</int>';
-                break;
             case 'double':
                 return '<double>'.$this->data.'</double>';
-                break;
             case 'string':
                 return '<string>'.htmlspecialchars($this->data).'</string>';
-                break;
             case 'array':
                 $return = '<array><data>'."\n";
                 foreach ($this->data as $item) {
@@ -87,7 +83,6 @@ class IXR_Value {
                 }
                 $return .= '</data></array>';
                 return $return;
-                break;
             case 'struct':
                 $return = '<struct>'."\n";
                 foreach ($this->data as $name => $value) {
@@ -96,11 +91,9 @@ class IXR_Value {
                 }
                 $return .= '</struct>';
                 return $return;
-                break;
             case 'date':
             case 'base64':
                 return $this->data->getXml();
-                break;
         }
         return false;
     }
