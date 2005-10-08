3a5f661640e08b2f64a4d8d12e780388c879a2cd
flyguide <flyguide@735d13b6-9817-0410-8766-e36946ffe9aa>
2005-10-08 23:01:36 +0000

    changes in trackback URLs
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/timtab/trunk@2562 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_timtab_trackback.php
+++ class.tx_timtab_trackback.php
@@ -328,6 +328,8 @@ class tx_timtab_trackback {
 	 * @return	string
 	 */
 	function getSourceURL() {
+		//FIXME respect tt_news useHRDate settings
+		
 		$urlParameters = array(
 			'tx_ttnews[year]'    => date('Y', $this->tt_news['datetime']),
 			'tx_ttnews[month]'   => date('m', $this->tt_news['datetime']),
@@ -347,9 +349,6 @@ class tx_timtab_trackback {
 	 */
 	function getTrackbackURL() {
 		$urlParameters = array(
-		/*	'tx_ttnews[year]'    => date('Y', $this->tt_news['datetime']),
-			'tx_ttnews[month]'   => date('m', $this->tt_news['datetime']),
-			'tx_ttnews[day]'     => date('d', $this->tt_news['datetime']),	*/
 			'tx_ttnews[tt_news]' => $this->tt_news['uid'],
 			'type'               => 200,
 			'tx_timtab_pi2[trackback]' => 1
@@ -367,9 +366,6 @@ class tx_timtab_trackback {
 	 */
 	function getTrackbackLink() {
 		$urlParameters = array(
-			'tx_ttnews[year]'    => date('Y', $this->tt_news['datetime']),
-			'tx_ttnews[month]'   => date('m', $this->tt_news['datetime']),
-			'tx_ttnews[day]'     => date('d', $this->tt_news['datetime']),
 			'tx_ttnews[tt_news]' => $this->tt_news['uid'],
 			'type'               => 200,
 			'tx_timtab_pi2[trackback]' => 1
