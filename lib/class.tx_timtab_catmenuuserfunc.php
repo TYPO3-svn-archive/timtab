da4077e77c138b420f82b509bba7239614455cc4
flyguide <flyguide@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-08-19 09:27:01 +0000

    removed file comment, fixed @subpackage annotation
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/timtab/trunk@36969 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib/class.tx_timtab_catmenuuserfunc.php
+++ lib/class.tx_timtab_catmenuuserfunc.php
@@ -21,21 +21,13 @@
 *
 *  This copyright notice MUST APPEAR in all copies of the script!
 ***************************************************************/
-/**
- * HMENU base category menu
- *
- * @package TYPO3
- * @subpackage tx_timtab
- * @author	Lina Wolf <2010@lotypo3.de>
- * @author	Timo Webler <timo.webler@dkd.de>
- * @version $Id$
- */
+
 
 /**
  * HMENU base category menu
  *
  * @package TYPO3
- * @subpackage tx_timtab
+ * @subpackage timtab
  * @author	Lina Wolf <2010@lotypo3.de>
  * @author	Timo Webler <timo.webler@dkd.de>
  */
